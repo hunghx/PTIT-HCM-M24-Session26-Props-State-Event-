c1f5ce01720ced7ae4064ad3fd4cf565f41822f8
hunghx <baby10051999@gmail.com>
2024-06-06 09:49:22 +0700

    ss26

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Form from "./components/Form"
+import Parent from "./components/Parent"
+
+function App() {
+ // thiết kế ứng dụng quản lí sinh viên   
+
+ // khai báo các thuộc tính 
+  return (
+    <div className="container">
+      {/* <Parent/> */}
+      <Form/>
+    </div>
+  )
+}
+
+export default App
