45e21c0cae7c63847aeec2aa3a3c6e785480c443
hunghx <baby10051999@gmail.com>
2024-06-10 09:57:59 +0700

    up

--- src/baitaptonghop/Calculator.tsx
+++ src/baitaptonghop/Calculator.tsx
@@ -1,37 +1,58 @@
-import React, { Component } from 'react'
+import React, { Component, MouseEventHandler } from 'react'
 import { Button } from 'react-bootstrap'
 
-export default class Calculator extends Component {
+export default class Calculator extends Component <{},{express :string[], result : number}> {
+    constructor(props: any){
+        super(props)
+        this.state = {
+            express : [],
+            result : 0
+        }
+    }
+    handleClickButton = (e : string) => {
+      this.setState({
+        express : [...this.state.express, e]
+      })
+    }
+    calExpress = ()=>{
+        let result =  eval(this.state.express.join(""))
+        console.log( result);
+        this.setState({
+            ...this.state,
+            result : result
+        })
+    }
   render() {
+    
     return (
       <div className='bg-danger container rounded-1 mt-4  p-2 border border-1 border-warning' style={{width: "200px"}}>
         <div className='bg-light mb-3 p-2 border-4 border rounded-2' style={{width:"100%"}}>
-            <span>1+2+3</span>
+            <span>{this.state.express.join("")}</span>
             <br />
-            <span className='fw-bold'>= 6</span>
+            <span className='fw-bold'>= {this.state.result}</span>
         </div>
         <div className="d-flex gap-2 mb-2 justify-content-between">
-            <div><Button variant='secondary'>1</Button></div>
-            <div><Button  variant='secondary'>2</Button></div>
-            <div><Button  variant='secondary'>3</Button></div>
-            <div><Button  variant='info'>+</Button></div>
+            <div><Button variant='secondary' onClick={()=>this.handleClickButton('1')}>1</Button></div>
+            <div><Button  variant='secondary' onClick={()=>this.handleClickButton('2')}>2</Button></div>
+            <div><Button  variant='secondary' onClick={()=>this.handleClickButton('3')}>3</Button></div>
+            <div><Button  variant='info' onClick={()=>this.handleClickButton('+')}>+</Button></div>
         </div>
         <div className="d-flex gap-2 mb-2 justify-content-between">
-            <div><Button variant='secondary'>4</Button></div>
-            <div><Button  variant='secondary'>5</Button></div>
-            <div><Button  variant='secondary'>6</Button></div>
-            <div><Button  variant='info'>-</Button></div>
+            <div><Button variant='secondary' onClick={()=>this.handleClickButton('4')}>4</Button></div>
+            <div><Button  variant='secondary' onClick={()=>this.handleClickButton('5')}>5</Button></div>
+            <div><Button  variant='secondary' onClick={()=>this.handleClickButton('6')}>6</Button></div>
+            <div><Button  variant='info'onClick={()=>this.handleClickButton('-')} >-</Button></div>
         </div>
         <div className="d-flex gap-2 mb-2 justify-content-between" >
-            <div><Button variant='secondary'>7</Button></div>
-            <div><Button  variant='secondary'>8</Button></div>
-            <div><Button  variant='secondary'>9</Button></div>
+            <div><Button variant='secondary' onClick={()=>this.handleClickButton('7')}>7</Button></div>
+            <div><Button  variant='secondary ' onClick={()=>this.handleClickButton('8')}>8</Button></div>
+            <div><Button  variant='secondary' onClick={()=>this.handleClickButton('9')}>9</Button></div>
             <div><Button  variant='info'>*</Button></div>
         </div>
         <div className="d-flex gap-2 justify-content-between" >
-            <div><Button variant='secondary'>0</Button></div>
+            <div><Button variant='secondary' onClick={()=>this.handleClickButton('0')}>0</Button></div>
             <div><Button  variant='secondary'>.</Button></div>
-            <div><Button >=</Button></div>
+            <div><Button onClick={this.calExpress} >=</Button></div>
             <div><Button  variant='info'>/</Button></div>
         </div>
       </div>
